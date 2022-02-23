bbbb06f42cc33d3cebe2d19db64b094554fb0148
bcaliva21 <bcaliva21@gmail.com>
2022-02-22 18:08:12 -0800

    clean up

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <h1>Home Page</h1>
             <p>
-                We are a group of faggots who love guns and america. Buy our NFT so we can be rich.
+                Buy our NFTs so we can be rich.
             </p>
         </div>
     )
