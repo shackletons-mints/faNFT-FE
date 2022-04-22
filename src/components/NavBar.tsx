876ea5e569d93f0a00b33923b495a3e26c4d7748
bcaliva21 <bcaliva21@gmail.com>
2022-04-21 19:43:14 -0700

    refactor(navbar): nav bar style cleanup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import logo from '../assets/fanft_logo.svg'
 import contract from '../assets/contract.svg'
 import home from '../assets/home.svg'
 import money from '../assets/money.svg'
+import ether from '../assets/ether.svg'
 
 const NavBar = () => {
 
@@ -12,25 +13,25 @@ const NavBar = () => {
             <span id='nav-title'>Double Dragon Crypto</span>
             <li>
                 <Link to='/' className='nav-link'>
-                    <img src={home}/>
+                    <img className='image' src={home}/>
                     <span className='nav-text'>Home</span>
                 </Link>
             </li>
             <li>
                 <Link to='/lottery' className='nav-link'>
-                    <img src={money}/>
+                    <img className='image' src={money}/>
                     <span className='nav-text'>Lottery</span>
                 </Link>
             </li>
             <li>
                 <Link to='/fanFT' className='nav-link'>
-                    <img src={logo}/>
+                    <img className='image' src={ether}/>
                     <span className='nav-text'>fanFT</span>
                 </Link>
             </li>
             <li>
                 <Link to='/contract' className='nav-link'>
-                    <img src={contract}/>
+                    <img className='image' src={contract}/>
                     <span className='nav-text'>Contract</span>
                 </Link>
             </li>
