fbbd37b523720018f86eebcb4e6cd7a1add5b3d0
bcaliva21 <bcaliva21@gmail.com>
2022-06-24 11:15:30 -0700

    (proxies): add put/post/get route for fans

--- src/components/routes/FunctionalMint.jsx
+++ src/components/routes/FunctionalMint.jsx
@@ -15,6 +15,16 @@ const FunctionalMint = (setCurrentAccount, currentAccount) => {
         bye: 'bye',
     }
 
+    const fanData = {
+        cid: 'QmNauibfNtgR5kLyF86UVZ6M18b8yJR9Qfx6qnHWXKEjnr',
+        title: 'CommonLeaf_KanagawaLeafName_Common2Handle_CooperHandleMaterial_EpicParticle_HeartsparticleEffect_ID-#8096.mp4',
+    }
+
+    const fanUpdateData = {
+        cid: 'QmNauibfNtgR5kLyF86UVZ6M18b8yJR9Qfx6qnHWXKEjnr',
+        ownerAddress: '0xC52fb4ce689989BacEB32Ce83f682842Ca018F41',
+    }
+
     // Let's send data like this
 
     // { payload: JSON_STRING }
@@ -31,12 +41,52 @@ const FunctionalMint = (setCurrentAccount, currentAccount) => {
           })
           .catch(function (error) {
             console.log(error);
-          });
+          })
     }
+
+    const insertFanIntoDatabase = async () => {
+        const config = {
+            headers: {
+                contentType: 'application/json',
+            }
+        }
+
+        axios.post('/fan', { payload: JSON.stringify(fanData) }, config)
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+    }
+
+    const updateFanInDatabase = async () => {
+        const config = {
+            headers: {
+                contentType: 'application/json',
+            }
+        }
+
+        axios.put('/fan', { payload: JSON.stringify(fanUpdateData) }, config)
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+    }
+    
+    const getFanByCid = async () => {
+        const config = {
+            headers: {
+                contentType: 'application/json'
+            }
+        }
+
+        axios.get('/get-fan?cid=QmNauibfNtgR5kLyF86UVZ6M18b8yJR9Qfx6qnHWXKEjnr', config)
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+    }
+
     return (
         <>
             <button onClick={() => connectWalletHandler(setCurrentAccount)}>Connect Wallet</button>
             <button onClick={testProxyRoute} >Mint</button>
+            <button onClick={insertFanIntoDatabase}>create fan</button>
+            <button onClick={updateFanInDatabase}>assign fan an owner</button>
+            <button onClick={getFanByCid}>get fan by cid</button>
         </>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,19 @@ export default defineConfig({
     // ...vite configures
     server: {
         proxy: {
-            '/test': {
+            '/get-fans': {
                 // target: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
                 target: 'http://localhost:8080',
                 changeOrigin: true,
             },
+            '/fan': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/get-fan': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
         },
         // vite server configs, for details see [vite doc](https://vitejs.dev/config/#server-host)
         port: 3000,
